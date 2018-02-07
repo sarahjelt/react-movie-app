fcf76e9b7c10a40ecf2fcca75a2a2d0af29958bb
Daniel Blat <blatde91@gmail.com>
2018-02-06 22:34:50 -0500

    work in progress

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,13 +1,48 @@
 import React from 'react';
 
 export const UserInfo = (props) => (
-    <div style={Style.container}>
-        <h2>Billy Bob</h2>
+    <div className="row blah">
+      <div>
+        <div className="col s12 m6">
+          <div className="card red lighten-1" style={Style.cardContainerLeft}>
+            <div className="card-content white-text">
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div>
+        <div className="col s12 m6">
+            <div className="card grey darken-1" style={Style.cardContainerRight}>
+                <div className="card-content white-text">
+                <span className="card-title" style={Style.cardTitle}>Billy Bob</span>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
 )
 
 const Style = {
     container: {
         paddingLeft: 20
+    },
+    cardContainerLeft: {
+        marginLeft: -15,
+        height: 190,
+        marginTop: -20,
+        marginRight: -5
+    },
+    cardContainerRight: {
+        height: 190,
+        marginTop: -20,
+        marginLeft: -5,
+        marginRight: -15
+    },
+    cardTitle: {
+        marginTop: 50,
+        textAlign: "right",
+        fontSize: 80,
+        marginRight: 100
     }
 }
--- client/src/components/UserModules.js
+++ client/src/components/UserModules.js
@@ -1,41 +1,48 @@
 import React from 'react';
 
 export const UserModules = (props) => (
-    <div className='userModules row' style={Style.container}>
-        <div className='col s4'>
-            <div style={Style.modules}>
-                <h5>Lists</h5>
-            </div>
+    <div className='row' style={Style.container}>
+      <div className='col s4'>
+        <div className='card blue'>
+          <div className='card-content'>
+            <span className='card-title' style={Style.titles}>Lists</span>
+          </div>
         </div>
-        <div className='col s4'>
-            <div style={Style.modules}>
-                <h5>Shelves</h5>
-            </div>
+      </div>
+      <div className='col s4'>
+        <div className='card lime'>
+          <div className='card-content'>
+            <span className='card-title' style={Style.titles}>Shelf</span>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
         </div>
-        <div className='col s4'>
-            <div style={Style.modules}>
-                <h5>Reviews</h5>
-            </div>
+      </div>
+      <div className='col s4'>
+        <div className='card teal'>
+          <div className='card-content'>
+            <span className='card-title' style={Style.titles}>Reviews</span>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
 )
 
 const Style = {
-    container: {
-        marginTop: 90,
-        marginBottom: '30px',
-        textAlign: 'center',
-        height: '400px',
-        overflow: 'hidden',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    modules: {
-        textAlign: 'left',
-        paddingLeft: 15,
-        border: '1px solid black',
-        width: '85%',
-        height: '400px'
-    }
+  titles: {
+      textAlign: "center",
+      color: "white"
+  }
+
+
 }
