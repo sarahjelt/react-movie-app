462efe07412c71fcff66ab4893bee01ca0d99b0e
Sara Hjelt <s.reb.thom@gmail.com>
2018-02-08 13:16:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ app.get('/authorized', function (req, res) {
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
