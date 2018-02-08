49d97c18fcfed49d7874e19f6348f38bbee769ba
Sara Hjelt <s.reb.thom@gmail.com>
2018-02-08 11:50:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("client/build"));
+
+
 //auth stuff
 app.use(cors());
 
@@ -29,9 +32,9 @@ const authCheck = jwt({
 });
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 app.use(routes);
 
@@ -65,9 +68,9 @@ app.get('/authorized', function (req, res) {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
