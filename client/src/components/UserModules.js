9151e12bce9d22e68d23dbacab0ac3fde33c5461
Daniel Blat <blatde91@gmail.com>
2018-02-09 12:01:13 -0500

    unstyled review modal

--- client/src/components/UserModules.js
+++ client/src/components/UserModules.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {ReviewModal} from './ReviewModal';
 
 export const UserModules = (props) => (
+  <div>
     <div className='row' style={Style.container}>
       <div className='col s4'>
         <div className='card blue'>
@@ -20,10 +22,16 @@ export const UserModules = (props) => (
         <div className='card teal'>
           <div className='card-content'>
             <span className='card-title' style={Style.titles}>Reviews</span>
+            <input href='#reviewModal' className='modal-trigger' type="button" onClick={() => props.reviewModalTrigger()} />
           </div>
         </div>
       </div>
     </div>
+    <ReviewModal value={props.value}
+                 reviewBool={props.reviewBool}
+                 handleReviewSubmit={props.handleReviewSubmit}
+                 handleReviewChange={props.handleReviewChange}/>
+  </div>
 )
 
 const Style = {
