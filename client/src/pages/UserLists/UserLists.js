5811fbdd205b500348f100a66a43bd3088a51f44
Lindsay Wall <lindsay.is@gmail.com>
2018-02-11 13:44:08 -0500

    userId and info put into state and pulling user info

--- client/src/pages/UserLists/UserLists.js
+++ client/src/pages/UserLists/UserLists.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import { UserModules } from '../../components/UserModules';
+import AuthService from '../../components/modules/AuthService';
+import decode from 'jwt-decode';
 
 class UserLists extends Component {
-  // state = {
+    constructor(props) {
+        super(props);
+        this.Auth = new AuthService();
+    }
 
-  // };
+    state = {
+        userLists: [],
+        userEmail: '',
+        userId: '',
+        userName: ''
+    };
 
-  // componentDidMount() {
+    componentWillMount() {
+        let userInfo = this.Auth.getProfile();
+        console.log(userInfo)
 
-  // };
+        if (!userInfo) {
+            window.location.replace("/")
+        } else {
+            this.setState({
+                userEmail: userInfo.email,
+                userId: userInfo._id,
+                userName: userInfo.name
+            })
+        }
+    }
 
 
   render() {
