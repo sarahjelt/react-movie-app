02ea0ff5ed076d03b03590b707c78905fda53c19
Sara Hjelt <s.reb.thom@gmail.com>
2018-02-11 23:22:40 -0500

    made reviews actually submit review, modules titles uniform

--- client/src/components/ListModal.js
+++ client/src/components/ListModal.js
@@ -5,31 +5,15 @@ export const ListModal = (props) => (
         <div className="card">
             <div className="card-content">
               <span className="card-title">New List</span>
-                <div onSubmit={(event) => props.handleReviewSubmit(event)}>
+                <div>
                     <label>
                         <input placeholder='Name your list' name='listName' value={props.listName} onChange={(event) => props.handleEventChange(event)}/>
                         <textarea placeholder='Please write list here' name='listValue' value={props.listValue} onChange={(event) => props.handleEventChange(event)}/>
                     </label>
-                    <a className='waves-effect waves-light btn cyan lighten-1' value="Add List" type="submit">Submit</a>
+                    <a onClick={(event) => props.handleListSubmit(event)} className='modal-close waves-effect waves-light btn cyan lighten-1' value="Add List" type="submit">Submit</a>
                 </div>
             </div>
         </div>
     </div>
 
-    //<div id='listModal' className={props.listBool === true ? 'modal open' : 'modal'}>
-    //<div className="row">
-    //    <div className="col s12 m5">
-    //        <div className="card-panel">
-    //            <span>Add a list</span>
-    //            <form onSubmit={(event) => props.handleListSubmit(event)}>
-    //                <label>List:
-    //                    <textarea placeholder='Name your list' name='listName' value={props.listName} onChange={(event) => props.handleEventChange(event)}/>
-    //                    <textarea placeholder='Please write list here' name='listValue' value={props.listValue} onChange={(event) => props.handleEventChange(event)}/>
-    //                </label>
-    //                <input type="submit" value="Submit" />
-    //            </form>
-    //        </div>
-    //    </div>
-    //</div>
-//</div>
 )
