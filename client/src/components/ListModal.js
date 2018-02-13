483e0b54b6a6dd1812f3d70aa57788b336e42198
Lindsay Wall <lindsay.is@gmail.com>
2018-02-12 20:37:13 -0500

    list and review modal labels and textarea resizing constraints

--- client/src/components/ListModal.js
+++ client/src/components/ListModal.js
@@ -6,24 +6,47 @@ export const ListModal = (props) => (
             <div className="card-content">
               <span className="card-title">New List</span>
                 <div>
-                    <label>
+                    <div style={Style.formContainer}>
+                        <label style={Style.label} htmlFor='listName'>NAME YOUR LIST</label>
                         <input 
                             placeholder='Name your list' 
                             name='listName' 
                             value={props.listName} 
                             onChange={(event) => props.handleEventChange(event)}
                         />
-                        <textarea 
+                        <label style={Style.label} htmlFor='listValue'>DECLARE YOUR LIST HERE</label>
+                        <textarea
+                            style={Style.textarea}
                             placeholder='Please write list here' 
                             name='listValue' 
                             value={props.listValue} 
                             onChange={(event) => props.handleEventChange(event)}
                         />
-                    </label>
-                    <a onClick={(event) => props.handleListSubmit(event)} className='modal-close waves-effect waves-light btn cyan lighten-1' value="Add List" type="submit">Submit</a>
+                    </div>
+                    <a style={Style.button} onClick={(event) => props.handleListSubmit(event)} className='modal-close waves-effect waves-light btn cyan lighten-1' value="Add List" type="submit">Submit</a>
                 </div>
             </div>
         </div>
     </div>
-
 )
+
+const Style = {
+    textarea: {
+        maxHeight: '300px',
+        resize: 'vertical',
+        minHeight: '100px',
+        border: '1px solid black'
+    },
+    label: {
+        fontSize: '16px',
+        paddingBottom: '5px',
+        letterSpacing: '1px'
+    },
+    formContainer: {
+        marginTop: '25px',
+    },
+    button: {
+        marginBottom: '25px',
+        marginTop: '25px'
+    }
+}
--- client/src/components/ReviewModal.js
+++ client/src/components/ReviewModal.js
@@ -6,14 +6,37 @@ export const ReviewModal = (props) => (
              <div className="card-content">
                <span className="card-title">New Review</span>
                 <div>
-                     <label>
+                     <div style={Style.formContainer}>
+                         <label style={Style.label} htmlFor="reviewName">HEADLINE</label>
                          <input placeholder='Name your review' name='reviewName' value={props.reviewName} onChange={(event) => props.handleEventChange(event)} />
-                         <textarea placeholder='Please write review here' name='reviewValue' value={props.reviewValue} onChange={(event) => props.handleEventChange(event)} />
-                     </label>
-                     <a onClick={(event) => props.handleReviewSubmit(event)} className='modal-close waves-effect waves-light btn cyan lighten-1' value="Add List" type="submit">Submit</a>
+                         <label style={Style.label} htmlFor="reviewValue">SAY IT</label>
+                         <textarea style={Style.textarea} placeholder='Please write review here' name='reviewValue' value={props.reviewValue} onChange={(event) => props.handleEventChange(event)} />
+                     </div>
+                     <a style={Style.button} onClick={(event) => props.handleReviewSubmit(event)} className='modal-close waves-effect waves-light btn cyan lighten-1' value="Add List" type="submit">Submit</a>
                 </div>
               </div>
             </div>
         </div>
 
-) 
+)
+
+const Style = {
+    textarea: {
+        maxHeight: '300px',
+        resize: 'vertical',
+        minHeight: '100px',
+        border: '1px solid black'
+    },
+    label: {
+        fontSize: '16px',
+        paddingBottom: '5px',
+        letterSpacing: '1px'
+    },
+    formContainer: {
+        marginTop: '25px',
+    },
+    button: {
+        marginBottom: '25px',
+        marginTop: '25px'
+    }
+}
