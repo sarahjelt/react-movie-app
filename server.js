e6768b92623109fe0ad1940b055ab67982333fe6
Lindsay Wall <lindsay.is@gmail.com>
2018-02-13 22:01:25 -0500

    Building for production

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(PORT, function() {
